14e1bbfabd830a23eb72217ee131798600fc5f65
Kirill <kiw197204@gmail.com>
2025-04-26 14:46:38 +0400

    feat: cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,7 +1,16 @@
-import env from 'node:process'
+import process from 'node:process'
 
 const parseArgs = () => {
+  for (let idx = 2; idx < process.argv.length; idx++) {
+    const el = process.argv[idx]
+    if (idx === process.argv.length - 1) {
+      process.stdout.write(`${el}\n`)
+    } else if (idx % 2) {
+      process.stdout.write(`${el}, `)
+    } else {
+      process.stdout.write(`${el} is `)
+    }
+  }
+}
 
-};
-
-parseArgs();
+parseArgs()
