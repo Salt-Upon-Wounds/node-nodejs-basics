56c3ed7ca987a2a7ff73507d130144737e581819
Kirill <kiw197204@gmail.com>
2025-04-28 13:04:37 +0400

    feat: zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
-const decompress = async () => {
-    // Write your code here 
-};
+import { createReadStream, createWriteStream } from 'node:fs'
+import path from 'node:path'
+import { createUnzip } from 'node:zlib'
+import { pipeline } from 'node:stream/promises'
+import getDirname from '../fs/dirname.js'
 
-await decompress();
+const decompress = async () => {
+  const gzip = createUnzip()
+  const filePath = path.join(getDirname(import.meta.url), 'files', 'fileToCompress.txt')
+  const arcPath = path.join(getDirname(import.meta.url), 'files', 'archive.gz')
+  const source = createReadStream(arcPath);
+  const destination = createWriteStream(filePath);
+  return pipeline(source, gzip, destination);
+}
+
+await decompress()
